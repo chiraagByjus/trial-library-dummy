3cfadcb4d61429d9e27a44208eb0b35b956e0354
Chiraag A <chiraag.a01@byjus.com>
2023-04-11 16:31:19 +0530

    removed recharts

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 import { CardWrapper } from "./components/CardWrapper";
-import { BarChartWrapper } from "./components/BarChartWrapper";
 import { VideoPlayer } from "./components/VideoPlayer";
 
 const returnLibrary = () => {
   return {
     CardWrapper: CardWrapper,
-    BarChartWrapper: BarChartWrapper,
     VideoPlayer: VideoPlayer,
   };
 };
