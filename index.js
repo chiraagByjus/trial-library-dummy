6b484eeebc855377d383d10f31d0714165e48f2f
Chiraag A <chiraag.a01@byjus.com>
2023-04-11 16:24:41 +0530

    added new components

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 import { CardWrapper } from "./components/CardWrapper";
+import { BarChartWrapper } from "./components/BarChartWrapper";
+import { VideoPlayer } from "./components/VideoPlayer";
 
 const returnLibrary = () => {
   return {
     CardWrapper: CardWrapper,
+    BarChartWrapper: BarChartWrapper,
+    VideoPlayer: VideoPlayer,
   };
 };
 export default returnLibrary();
